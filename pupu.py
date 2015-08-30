5392e6b35a4970d2f4b45b61fba19b9cebb15c46
Song Jaehak <hpi@moonrabbit.co>
2015-08-30 16:37:52 +0900

    1바이트 줄임

--- pupu.py
+++ pupu.py
@@ -1,7 +1,6 @@
 #coding:L1
 d,m='',0
-t=['y“x–w—v™vSJv¤v¥w¥hNN¥fPP¢cSQ¡cSP ]^JŸY²V´T¸SºR¼Q½P¿PÀOSIUIŸOPIJHOHJHOÁPÀPÀPÀNÄMÇLdHMH KVLQM¢JËJËJËJËKÉLÇMÆNÄOÁR¼V³']
-if str!=bytes:t+=['L1']
-for c in bytearray(*t):
+t=['y“x–w—v™vSJv¤v¥w¥hNN¥fPP¢cSQ¡cSP ]^JŸY²V´T¸SºR¼Q½P¿PÀOSIUIŸOPIJHOHJHOÁPÀPÀPÀNÄMÇLdHMH KVLQM¢JËJËJËJËKÉLÇMÆNÄOÁR¼V³','L1']
+for c in bytearray(*t[0:(str!=bytes)+1]):
  d+=c%70*" *"[m];m=(m+1)%2
  if c>140:print(d);d=''
